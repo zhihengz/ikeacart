3eed67fc6269ceb8f98eb04c9f9559668e4755f3
Jason Zhang <zhihengz@gmail.com>
2009-09-05 22:54:47 -0500

    skeleton code to support rate image

--- src/org/longhorn/android/ikeacart/RowViewBinder.java
+++ src/org/longhorn/android/ikeacart/RowViewBinder.java
@@ -3,6 +3,7 @@ package org.longhorn.android.ikeacart;
 import android.database.Cursor;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.ImageView;
 import android.widget.SimpleCursorAdapter.ViewBinder;
 
 public class RowViewBinder implements ViewBinder {
@@ -22,17 +23,34 @@ public class RowViewBinder implements ViewBinder {
 	    quantityTextView.setText( " (" + cursor.getInt( col ) + ")");
 	    return true;
 	} else if ( colPriority == col ) {
-	    TextView priorityTextView = (TextView)view;
-	    int priority = cursor.getInt( col );
-	    StringBuilder buf = new StringBuilder( "");
-	    for ( int i = 0; i < priority; i++ ) {
-		buf.append( "* " );
-	    }
-	    priorityTextView.setText( buf.toString());
+	    setupPriorityView( cursor.getInt( col ), view );
 	    return true;
 	}
 	
 	return false;
     }
-				     
+    private void setupPriorityView( int priority, View view ) {
+
+	TextView priorityTextView = (TextView)view;
+	StringBuilder buf = new StringBuilder( "");
+	for ( int i = 0; i < priority; i++ ) {
+	    buf.append( "* " );
+	}
+	priorityTextView.setText( buf.toString());
+    }
+    /**
+    private void setupPriorityView( int priority, View view ) {
+
+	ImageView priorityView = (ImageView)view;
+	StringBuilder buf = new StringBuilder( "");
+	for ( int i = 0; i < priority; i++ ) {
+	    buf.append( "* " );
+	}            
+	priorityView.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
+
+	priorityView.setImageResource( R.drawable.rate_4 );
+	
+    }
+    **/
+    
 }
